ea4386884ba0093fa1bed8ca51327cdae9cb6e98
ShashankSBysani <shashanksbysani@gmail.com>
2024-03-30 23:27:46 +0530

    version1.4

--- RENTAL APP.C
+++ RENTAL APP.C
@@ -1,23 +1,58 @@
-// rental service application
-#include<stdio.h> //header files
-#include<string.h> //string hedar files
-// Function to rent a car
+/*Rental Service Application
+
+*This APP Is Used For Renting Car 
+
+*This App Gives You A Wide Varity Of Cars To Chose
+
+*This Also Has Differnt Pricing For Differnt Cars.*/
+
+
+
+#include<stdio.h> 
+#include<string.h> //String Headar Files
+
+// Used To Store Customer Details
+struct details {
+    char customer_name[50];
+    long long int number;
+    int pincode;
+};
+//Used To Store Car Brands
+struct store {
+    char car_name[20];
+    struct details customer;
+};
+
+// Void Function To Rent A Car
 void car_rent()
 {
-    char car_Brand[6][20]={"FORD","TATA","MERCEDES","AUDI","ISUZU"};//brands available is used to stored in array
+//brands available is used to stored in array
+    char car_Brand[6][20]={"FORD","TATA","MERCEDES","AUDI","ISUZU"};
     int RENT[6]={250,500,650,750,850};
     char user_brand[20]; // to store the input of user array is used
 	unsigned int days=0;
 	int RENT_PRICE;
-	//TAKE INPUT FROM USER TO ASK HOW MANY DAYS USING SCAN 
+	struct store rented;
+	
+	
+	//TAKES INPUT FROM CUSTOMER
+    printf("Enter car brand: ");
+    scanf("%s", rented.car_name);
+    printf("Enter customer name: ");
+    gets(rented.customer.customer_name);
+    fgets(rented.customer.customer_name, sizeof(rented.customer.customer_name), stdin);
+    printf("Enter customer number: ");
+    scanf("%lld", &rented.customer.number);
+    printf("Enter customer pincode: ");
+    scanf("%d", &rented.customer.pincode);
 	printf("NO OF DAYS YOU WANT TO RENT:");
 	scanf("%u",&days);
 	printf("BRANDS AVAILABLE");
 	printf("\n'FORD','TATA'',''MERCEDES','AUDI'','ISUZU'\n");// these are the brands available for the renting of cars
 	printf("|*|PER DAY RENT DEPEND ON CAR BRAND|*|\n ||FORD       -  250|| \n ||TATA       -  500|| \n ||MERCEDES   -  650|| \n ||AUDI       -  750|| \n ||ISUZU      -  850|| ");//PRICE OF THE CAR HAS DIFFERENT PRICES
-	printf("\nENTER THE CAR BRAND NAME: ");
-	scanf("%s",user_brand);
+    strcpy(user_brand, rented.car_name);
 	int found = 0;
+	
 	// for loop is used to check every element in array
     for(int i = 0; i < 5; i++)
 	 {
@@ -27,6 +62,10 @@ void car_rent()
             found = 1;
              RENT_PRICE=days*RENT[i];
             printf("\nTOTAL RENT: %d",RENT_PRICE);
+            printf("\nCar brand: %s\n", rented.car_name);
+            printf("Customer name: %s\n", rented.customer.customer_name);
+            printf("Customer number: %lld\n", rented.customer.number);
+            printf("Customer pincode: %d\n", rented.customer.pincode);
             break;
         }
     }
@@ -35,10 +74,15 @@ void car_rent()
         printf("PLEASE SELECT OTHER CAR BRANDS");
     }
 }
+
 //FUNCTION TO RETURN CAR
 void car_return()
 {
 	printf("car returned.\n");
+}
+void display_available_car()
+{
+	printf("Available Car For Rent is\n");
 }
 int main()
 {
@@ -48,8 +92,11 @@ int main()
 	do{
 	
 
-	printf("\n TO RENT  PRESS 1\n TO RETURN PRESS 2\n PRESS 3 TO EXIT \n");
+	printf("\n TO RENT  PRESS 1\n TO RETURN PRESS 2\n PRESS 3 TO DISPLAY AVAILABLE CARS \n PRESS 4 TO EXIT \n");
 	scanf("%d",&x);//'x'Receive The Input From Customer//
+	
+	
+	//Switch Case Statement Is Used For Menu
 	switch(x)
 	{
 		case 1: 
@@ -58,13 +105,16 @@ int main()
 		case 2: 
 		car_return();
 			break;
-		case 3:
+	    case 3:
+	    	display_available_car();
+	    	break;
+		case 4:
     printf("Thank you for using our  application 1.1\n");
     break;
 		default:
 		printf("WRONG CHOICE!! Try again.\n");	
 	}
 }
-while(x!=3);{
+while(x!=4);{
 }
 }
